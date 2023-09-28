3f9111ce8308f1e4c8e8e0ac2960a5462c4725d5
EmilyHott4 <112112058+EmilyHott4@users.noreply.github.com>
2023-09-27 22:09:04 -0500

    draw the button to screen

--- button.py
+++ button.py
@@ -6,6 +6,7 @@ class Button:
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         
+        
 
         #set the dimensions and properties of the button
         self.width, self.height = 200, 50

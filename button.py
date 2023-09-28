95c8d8858c3a2e06618fdf8c89bd7eacd43e0502
EmilyHott4 <112112058+EmilyHott4@users.noreply.github.com>
2023-09-27 22:06:50 -0500

    adding button

--- button.py
+++ button.py
@@ -5,6 +5,7 @@ class Button:
         """initialize button attributes"""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
+        
 
         #set the dimensions and properties of the button
         self.width, self.height = 200, 50
